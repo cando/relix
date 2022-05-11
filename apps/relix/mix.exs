b65552b90c150d2436a1caef0eeb4ef2feed4d45
Stefano <stefano.candori@prima.it>
2022-05-11 14:47:41 +0200

    Moving to umbrella app

---
+++ apps/relix/mix.exs
@@ -0,0 +1,47 @@
+defmodule Relix.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :relix,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      aliases: aliases()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Relix.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:mox, "~> 1.0", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      test: [
+        "ecto.drop --quiet",
+        "ecto.create --quiet",
+        "ecto.migrate --quiet",
+        "test --include database"
+      ]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test"]
+  defp elixirc_paths(_), do: ["lib"]
+end
--- mix.exs
+++ mix.exs
@@ -1,51 +1,23 @@
-defmodule Relix.MixProject do
+defmodule RelixUmbrella.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :relix,
+      apps_path: "apps",
       version: "0.1.0",
-      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env()),
-      aliases: aliases(),
-      preferred_cli_env: [
-        "test.integration": :test
-      ]
+      deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {Relix.Application, []}
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
   defp deps do
     [
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:mox, "~> 1.0", only: :test},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
-
-  defp aliases do
-    [
-      "test.integration": [
-        "ecto.drop --quiet",
-        "ecto.create --quiet",
-        "ecto.migrate --quiet",
-        "test --include database"
-      ]
-    ]
-  end
-
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test"]
-  defp elixirc_paths(_), do: ["lib"]
 end
