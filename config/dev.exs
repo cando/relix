96d1381e852d546c4df82362684a85c0e2c8ec4b
Stefano <stefano.candori@prima.it>
2022-05-11 11:58:17 +0200

    Use db also in dev

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :relix, :recipe_store, Relix.RecipeStore.InMemoryStore
+config :relix, :recipe_store, Relix.RecipeStore.PostgresStore
 config :logger, level: :debug
 
 config :relix, Relix.RecipeStore.PostgresStore.Repo,
